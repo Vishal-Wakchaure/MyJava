5cb42bccaa318a7a21e6b2a62ecf2f0fb3f07f4e
Vishal Wakchaure <wakchaurev00@gmail.com>
2026-03-12 16:25:58 +0530

    Java programming

---
+++ src/Loops/Ass1.java
@@ -0,0 +1,31 @@
+package Loops;
+
+public class Ass1 {
+	public static void main(String[] args) {
+		//	Print numbers from 1 to 10. (Using for loop)
+		
+//		for(int i=1;i<=10;i++)
+//		{
+//			System.out.println(i);
+//		}
+		
+//		Print numbers from 10 to 1 (reverse order).
+		
+//		for(int i=10;i>=0;i--)
+//		{
+//			System.out.println(i);
+//		}
+		
+//		Print even numbers from 1 to 50.
+		
+//		for(int i=0;i<=50;i+=2)
+//		{
+//		   System.out.println(i);
+//		
+//		}
+		
+		
+		
+		
+	}
+}
--- src/genericsandwrapperclasses/WrapperClasses.java
+++ src/genericsandwrapperclasses/WrapperClasses.java
@@ -4,18 +4,38 @@ public class WrapperClasses {
 	
 	public static void main(String[] args) {
 		
-		Integer obj = new Integer(12);
+//		Integer obj = new Integer(12);
+//		
+//		Integer obj2 = Integer.valueOf(12);
+//		System.out.println(obj);
+//		System.out.println(obj2);
+//		
+//		Integer obj3 =10;//Autoboxing
+//		
+//		int age = obj; //unboxing
+//		
+//		Boolean myboolean = Boolean.valueOf(false);
+//		System.out.println(myboolean);
 		
-		Integer obj2 = Integer.valueOf(12);
-		System.out.println(obj);
-		System.out.println(obj2);
+//		Long e = 500L;
+//		int m = e.intValue();//non primitive to primitive
+//		System.out.println("m" +m);
+//		
+//		Double d = 5.4;
+//		float f = d.floatValue();
+//		System.out.println("f" +f);
 		
-		Integer obj3 =10;//Autoboxing
+//		int a =1234;
+//		String s = String.valueOf(a);//convert non primitive to primitive
+//		Double d = Double.valueOf(a);
+//		
+//		System.out.println(s);
+//		System.out.println(d);
 		
-		int age = obj; //unboxing
+		Integer a = 12345;
+		String s = a.toString();
+		System.out.println(s);
 		
-		Boolean myboolean = Boolean.valueOf(false);
-		System.out.println(myboolean);
 	}
 
 }
