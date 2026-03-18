c8791d1ee9f1b9ffcf6d269e1376c09ae0296084
Vishal Wakchaure <wakchaurev00@gmail.com>
2026-03-18 10:50:39 +0530

    Practiced Java Programs

--- src/strings/StringConstructor.java
+++ src/strings/StringConstructor.java
@@ -8,15 +8,51 @@ public class StringConstructor {
 	
 	public static void main(String[] args) {
 		
-		System.out.println(s1);
+//		System.out.println(s1);
 		
 //		System.out.println(System.identityHashCode(s));
 //		System.out.println(System.identityHashCode(s1));
 		
 //		char cha[] = new char[] {'a','b','c','d','e','f'};//accept character array as parameter & convert into string object
-		String s2 = new String(cha);
-		System.out.println(s2);
+//		String s2 = new String(cha);
+//		System.out.println(s2);		
 		
-
+//		char ch[] = new char[] {'a','b','c','d','e','f','g','h','i','j','k','l'};
+//		String s = new String (ch,2,6);
+//		System.out.println(s);
+		
+//		byte b[] = new byte[] {97,98,99,100};//convert byte array or ascii value to string
+//		String s = new String(b);
+//		System.out.println(s);
+		
+//		String s ="abcde";
+//		int len = s.length();
+//		for(int i=0;i<len;i++)
+//		{
+//			char ch = s.charAt(i);
+//			System.out.printf("s[%d] --->%c\n",i,ch);
+//		}
+		
+//		Calculate length of string without using length() function 
+//		String s = "abcde";
+//		int count =0;
+//		try {
+//			while(true)
+//			{
+//				s.charAt(count);
+//				++count;
+//			}
+//		}
+//		catch(Exception ex)
+//		{
+//			System.out.println("Length of string is "+count);
+//		}
+		
+//		String toUpperCase()-This method can convert lower case string to upper case.
+		
+		String str = "good";
+		String s1 = str.toUpperCase();
+		System.out.println(s1);
+		
 }
 }
